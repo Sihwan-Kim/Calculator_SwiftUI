18d766667c0e52414f884910536557b94de76a52
Sihwan Kim <sihwan72@gmail.com>
2020-03-21 13:12:13 +0900

    2020-03-21

--- Calculator_SwiftUI/ContentView.swift
+++ Calculator_SwiftUI/ContentView.swift
@@ -8,11 +8,92 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .foregroundColor(.yellow)
+struct ContentView: View
+{
+    @State var firstValue = "0"
+    
+    var body: some View
+    {
+        NavigationView
+        {
+            VStack
+            {
+                Form
+                {
+                    Text(firstValue)
+                    .bold()
+                    .font(.title)
+                }.navigationBarTitle("Calculator")
+                VStack
+                {
+                    HStack
+                    {
+                        Button("1")
+                        {
+                        
+                        }
+                        .frame(minWidth: 10, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.red)
+                        
+                        Button("2")
+                        {
+                            
+                        }
+                        Button("3")
+                        {
+                            
+                        }
+                    }
+                    HStack
+                    {
+                        Button("3")
+                        {
+                        
+                        }
+                        .frame(minWidth: 10, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.red)
+                        
+                        Button("4")
+                        {
+                            
+                        }
+                        Button("5")
+                        {
+                            
+                        }
+                    }
+                    HStack
+                    {
+                        Button("0")
+                        {
+                        
+                        }
+                        .frame(minWidth: 10, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.red)
+                        
+                        Button("1")
+                        {
+                            
+                        }
+                        Button("2")
+                        {
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
+}
+
+func AccumulateValue(inputNum: Int, currentNum: String)->String
+{
+    var temp = " "
+    
+    return temp
 }
 
 struct ContentView_Previews: PreviewProvider {
