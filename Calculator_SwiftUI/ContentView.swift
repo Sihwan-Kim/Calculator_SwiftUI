7eef6aff1630f2c9c87ec849b2ae765f28560913
Sihwan Kim <sihwan72@gmail.com>
2020-03-24 00:33:53 +0900

    2020-03-24

--- Calculator_SwiftUI/ContentView.swift
+++ Calculator_SwiftUI/ContentView.swift
@@ -8,51 +8,29 @@
 
 import SwiftUI
 
-struct NumberView: View
-{
-    let number: Double
-    var numberString: String
-    {
-        if number == .pi
-        {
-            return "ℿ"
-        }
-        
-        return String(Int(number))
-    }
-    
-    var body: some View
-    {
-        Text(numberString)
-            .font(.title)
-            .fontWeight(.bold)
-            .foregroundColor(.white)
-            .frame(width:64, height: 64)
-            .background(Color.blue)
-            .cornerRadius(20)
-            .shadow(color: Color.blue.opacity(0.3), radius: 10, x: 0, y: 10)
-    }
-}
-
 struct GradientButtonStyle: ButtonStyle
 {
-    var buttonWidth: CGFloat=64
+    var buttonWidth: CGFloat=90
+    var forgroundColor: Color = Color.gray
     
     func makeBody(configuration: Self.Configuration) -> some View
     {
         configuration.label
             .foregroundColor(Color.white)
-            .frame(width:buttonWidth, height: 64)
-            .background(configuration.isPressed ? Color.yellow : Color.blue)
-            .cornerRadius(90)
+            .frame(width:buttonWidth, height: 90)
+            .background(configuration.isPressed ? Color.yellow : forgroundColor)
+            .cornerRadius(20)
             .shadow(color: Color.blue.opacity(0.3), radius: 10, x: 0, y: 10)
     }
 }
 
 struct ContentView: View
 {
-    @State var currentNumber: Double = 0
-    @State var firstNumber = "0"
+    @State var currentNumber =  "0"
+    @State var firstNumber: Double = 0
+    @State var Operand = 0 ;  // 1:+, 2:-, 3:X, 4:/
+    @State var pointExist = false
+    
     var displayString: String
     {
         return String(format: "%.2f", arguments:[currentNumber])
@@ -63,23 +41,146 @@ struct ContentView: View
     //    VStack(alignment: .trailing, spacing: 20)
         VStack
         {
-            Text(firstNumber)
+            Text(currentNumber)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .lineLimit(3)
                 .padding()
+            
+            HStack
+            {
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 1, currentNum: self.currentNumber) })
+                {
+                    Text("AC")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.green))
+                
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 2, currentNum: self.currentNumber) })
+                {
+                    Text("Pi")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.green))
+                
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 1, currentNum: self.currentNumber) })
+                {
+                    Text("%")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.green))
+                
+                Button(action:
+                {
+                    self.Operand = 4 ;
+                    self.firstNumber = Double(self.currentNumber)!
+                    self.currentNumber = "0"
+                    self.pointExist = false
+                })
+                {
+                    Text("÷")
+                        .font(.title)
+                        .fontWeight(.black )
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.orange))
+            }
+            
+            HStack
+            {
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 7, currentNum: self.currentNumber) })
+                {
+                    Text("7")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 8, currentNum: self.currentNumber) })
+                {
+                    Text("8")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 9, currentNum: self.currentNumber) })
+                {
+                    Text("9")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action:
+                {
+                    self.Operand = 3 ;
+                    self.firstNumber = Double(self.currentNumber)!
+                    self.currentNumber = "0"
+                    self.pointExist = false
+                })
+                {
+                    Text("×")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.orange))
+            }.padding()
+            
+            HStack
+            {
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 4, currentNum: self.currentNumber) })
+                {
+                    Text("4")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 5, currentNum: self.currentNumber) })
+                {
+                    Text("5")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 6, currentNum: self.currentNumber) })
+                {
+                    Text("6")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action:
+                {
+                    self.Operand = 2 ;
+                    self.firstNumber = Double(self.currentNumber)!
+                    self.currentNumber = "0"
+                    self.pointExist = false
+                })
+                {
+                    Text("-")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.orange))
+            }
                 
             HStack
             {
-                Button(action: {  self.firstNumber = AccumulateValue(inputNum: 1, currentNum: self.firstNumber) })
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 1, currentNum: self.currentNumber) })
                 {
                     Text("1")
                         .font(.title)
                         .fontWeight(.bold)
 
-                }.buttonStyle(GradientButtonStyle(buttonWidth: 120))
+                }.buttonStyle(GradientButtonStyle())
                 
-                Button(action: {  self.firstNumber = AccumulateValue(inputNum: 2, currentNum: self.firstNumber) })
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 2, currentNum: self.currentNumber) })
                 {
                     Text("2")
                         .font(.title)
@@ -87,7 +188,65 @@ struct ContentView: View
 
                 }.buttonStyle(GradientButtonStyle())
                 
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 3, currentNum: self.currentNumber) })
+                {
+                    Text("3")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action:
+                {
+                    self.Operand = 1 ;
+                    self.firstNumber = Double(self.currentNumber)!
+                    self.currentNumber = "0"
+                    self.pointExist = false
+                    
+                })
+                {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.orange))
+            }.padding()
+            
+            HStack
+            {
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 0, currentNum: self.currentNumber) })
+                {
+                    Text("0")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(buttonWidth: 188))
+                                
+                Button(action:
+                {
+                    if (self.pointExist == false)
+                    {
+                        self.currentNumber += "."
+                        self.pointExist = true
+                    }
+                    
+                })
+                {
+                    Text(".")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle())
+                
+                Button(action: {  self.currentNumber = AccumulateValue(inputNum: 2, currentNum: self.currentNumber) })
+                {
+                    Text("=")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                }.buttonStyle(GradientButtonStyle(forgroundColor: Color.orange))
             }
+
         }.padding(32)
         
 
@@ -96,9 +255,28 @@ struct ContentView: View
 
 func AccumulateValue(inputNum: Int, currentNum: String)->String
 {
-    let temp = currentNum + String(inputNum)
+    var result: String
     
-    return temp
+    if(currentNum == "0")
+    {
+        result = "" + String(inputNum)
+    }
+    else
+    {
+        result = currentNum + String(inputNum)
+    }
+    
+    return result
+}
+
+func Calculate(first:Double, second:Double, operator:Int)->Double
+{
+    var result:Double
+    
+    switch(operator)
+    {
+    case 1: 
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
